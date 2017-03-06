4c55d900bdd48814e7dee6be59ec74a448acab81
TomaszDM <paruzel.tomasz@gmail.com>
2017-03-06 20:34:57 +0100

    ogarnij

--- Pacman.pas
+++ Pacman.pas
@@ -512,7 +512,7 @@ const
 vdg=5;
 
 
-procedure gonitwa (vxd, vyd,kd, vxdp,vydp:integer);
+procedure gonitwa (vxd,vyd,kd,vxdp,vydp:integer);
 const
 
 deltax=16;
@@ -521,6 +521,8 @@ deltay=17;
 var
 xright,xleft,yup,ydown : boolean;
 deltagx,deltagy, R : shortint;
+x1t,x2t,y1t,y2t:string;
+
 
 begin
  // randomize;
@@ -531,8 +533,8 @@ begin
   begin
   vxd:=vxd+230;
   vyd:=vyd+202;
-//  vxdp:=vxd;
-//  vydp:=vyd;
+  vxdp:=vxd;
+  vydp:=vyd;
 
   xright:=true;  //do zmiany
   xleft:=true;
@@ -543,6 +545,57 @@ begin
   if (getpixel((vxd)-deltax,vyd)) <> kk then xleft:=false;
   if (getpixel((vxd),vyd+deltay)) <> kk then ydown:=false;
   if (getpixel((vxd),vyd-deltay)) <> kk then yup:=false;
+
+  if (getpixel((vxd)+deltax,vyd+5)) <> kk then xright:=false;  // do zmiany
+  if (getpixel((vxd)-deltax,vyd+5)) <> kk then xleft:=false;
+  if (getpixel((vxd+5),vyd+deltay)) <> kk then ydown:=false;
+  if (getpixel((vxd+5),vyd-deltay)) <> kk then yup:=false;
+
+  if (getpixel((vxd)+deltax,vyd+10)) <> kk then xright:=false;  // do zmiany
+  if (getpixel((vxd)-deltax,vyd+10)) <> kk then xleft:=false;
+  if (getpixel((vxd+10),vyd+deltay)) <> kk then ydown:=false;
+  if (getpixel((vxd+10),vyd-deltay)) <> kk then yup:=false;
+
+  if (getpixel((vxd)+deltax,vyd+15)) <> kk then xright:=false;  // do zmiany
+  if (getpixel((vxd)-deltax,vyd+15)) <> kk then xleft:=false;
+  if (getpixel((vxd+15),vyd+deltay)) <> kk then ydown:=false;
+  if (getpixel((vxd+15),vyd-deltay)) <> kk then yup:=false;
+
+  if (getpixel((vxd)+deltax,vyd-5)) <> kk then xright:=false;  // do zmiany
+  if (getpixel((vxd)-deltax,vyd-5)) <> kk then xleft:=false;
+  if (getpixel((vxd-5),vyd+deltay)) <> kk then ydown:=false;
+  if (getpixel((vxd-5),vyd-deltay)) <> kk then yup:=false;
+
+  if (getpixel((vxd)+deltax,vyd-10)) <> kk then xright:=false;  // do zmiany
+  if (getpixel((vxd)-deltax,vyd-10)) <> kk then xleft:=false;
+  if (getpixel((vxd-10),vyd+deltay)) <> kk then ydown:=false;
+  if (getpixel((vxd-10),vyd-deltay)) <> kk then yup:=false;
+
+  if (getpixel((vxd)+deltax,vyd-15)) <> kk then xright:=false;  // do zmiany
+  if (getpixel((vxd)-deltax,vyd-15)) <> kk then xleft:=false;
+  if (getpixel((vxd-15),vyd+deltay)) <> kk then ydown:=false;
+  if (getpixel((vxd-15),vyd-deltay)) <> kk then yup:=false;
+
+
+  setcolor(red);
+ // bar(900,200,1400,900);
+
+  setcolor(black);
+
+  str(xright,x1t);
+  str(xleft,x1t);
+  str(yup,x1t);
+  str(ydown,x1t);
+
+  setcolor(white);
+  settextstyle(TriplexFont,HorizDir,3);
+
+  outtextxy(900,200,x1t);
+  outtextxy(900,400,x2t);
+  outtextxy(900,600,y1t);
+  outtextxy(900,800,y2t);
+
+
 
   if (xright) and (not xleft) and (not yup) and (not ydown) then  //do zmiany
      begin
